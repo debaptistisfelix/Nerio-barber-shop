4a91de5af3b1c4de89bcb15a11b2a61b5e5adac6
Felix De Baptistis <debaptistisfelix@gmail.com>
2023-12-02 12:01:08 +0100

    changed parag of about us

--- app/COMPONENTS/AboutUs/AboutUs.jsx
+++ app/COMPONENTS/AboutUs/AboutUs.jsx
@@ -8,16 +8,10 @@ export default function AboutUs() {
       <main className={styles.aboutUs}>
         <h1 className={styles.title}>IL TUO BARBIERE DI FIDUCIA</h1>
         <p className={styles.intro}>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam, sed
-          sapiente nobis, at facere est dignissimos maxime, exercitationem
-          tempora optio rerum architecto quia itaque libero cum! Distinctio
-          porro molestiae possimus.
+        Benvenuti al nostro barber shop, un luogo dove la tradizione della barberia si fonde con l'innovazione moderna. Con anni di esperienza alle spalle, ci dedichiamo a fornire tagli personalizzati e trattamenti di alta qualità che rispecchiano le esigenze individuali di ogni cliente.
         </p>
         <p className={styles.intro}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
-          suscipit beatae non illum illo distinctio pariatur deserunt maiores at
-          commodi, aperiam tenetur perspiciatis quis impedit sequi facere atque!
-          Laudantium, facere!
+        La nostra missione è quella di offrirvi un'esperienza che vi faccia sentire al meglio, in un ambiente che è allo stesso tempo accogliente e rilassante. Affidatevi alla nostra competenza per il vostro prossimo look e scoprite il piacere di un servizio su misura.
         </p>
       </main>
       <Openings />
